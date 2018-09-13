db70baae8e7d8965f1021cee6d1364d70300911f
anneprt <anneprt>
2018-09-13 17:09:25 +0200

    création des models et de la classe Context

--- TodoList/Models/Categorie.cs
+++ TodoList/Models/Categorie.cs
@@ -5,13 +5,9 @@ using System.Web;
 
 namespace TodoList.Models
 {
-    public class Tache
+    public class Categorie
     {
         public int ID { get; set; }
         public string Nom { get; set; }
-        public string Description { get; set; }
-        public DateTime DateFin { get; set; }
-        public bool Statut { get; set; }
-        public int Priorite { get; set; }
     }
 }
