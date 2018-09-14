2c2982e5da10aa75e6adeba832c63b434578c612
anneprt <anneprt>
2018-09-14 15:40:32 +0200

    Ajout des methodes put et delete dans CategoriesController

--- TodoList/Controllers/CategoriesController.cs
+++ TodoList/Controllers/CategoriesController.cs
@@ -20,7 +20,49 @@ namespace TodoList.Controllers
 
 		public IHttpActionResult PostCategories(Categorie categorie)
 		{
-			db.Categories.Add(categorie);
+			if (ModelState.IsValid)
+			{
+				db.Categories.Add(categorie);
+				db.SaveChanges();
+
+				return Ok(categorie);
+			}
+			else
+				return BadRequest(ModelState);
+
+		}
+		public IHttpActionResult PutCategories(int id, Categorie categorie)
+		{
+			if (id != categorie.ID)
+			{
+				return BadRequest();
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (db.Categories.Count(x => x.ID == id) != 1)
+			{
+				return BadRequest();
+			}
+
+			db.Entry(categorie).State = System.Data.Entity.EntityState.Modified;
+			db.SaveChanges();
+
+			return StatusCode(HttpStatusCode.NoContent);
+		}
+
+		public IHttpActionResult DeleteCategories(int id)
+		{
+			var categorie = db.Categories.Find(id);
+			if (categorie ==null)
+			{
+				return NotFound();
+			}
+
+			db.Entry(categorie).State = System.Data.Entity.EntityState.Deleted;
 			db.SaveChanges();
 
 			return Ok(categorie);
